daed57eb2abb6f99f578f2a1208ba3c0a01486b9
deltaA380 <walsli@outlook.com>
2018-01-18 15:44:22 -0600

    Code Review
    Added Consts class.

---
+++ src/org/usfirst/frc/team3630/robot/Consts.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc.team3630.robot;
+
+public class Consts {
+	
+	// naming file convention can't reemeb
+	
+	// public static final int????
+	
+	// class secions  or organize by other catagories
+    // global assignments /
+	public static final int xBoxComPort = 0; 
+	// DriveTrain 
+	public static final int frontLeftTalon = 1;
+	public static final int backLeftTalon = 3;
+	public static final int frontRightTalon = 4; 
+	public static final int backRightTalon = 2;
+	
+	// Manipulators 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
--- src/org/usfirst/frc/team3630/robot/DriveTrain.java
+++ src/org/usfirst/frc/team3630/robot/DriveTrain.java
@@ -8,17 +8,18 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 public class DriveTrain {
 	private XboxController _xBox;
 	// add coment about from what perspective of robot 
+	
 	private WPI_TalonSRX frontLeft, frontRight, backLeft, backRight;
 	private SpeedControllerGroup rightSpeedController, leftSpeedController;
 	private DifferentialDrive driveTrain;
 	
 	public DriveTrain() {
 		// conts classss !!!!!!
-		_xBox = new XboxController(0);
-		frontLeft = new WPI_TalonSRX(1);
-		backLeft = new WPI_TalonSRX(3);
-		frontRight = new WPI_TalonSRX(4);
-		backRight = new WPI_TalonSRX(2);
+		_xBox = new XboxController(Consts.xBoxComPort);
+		frontLeft = new WPI_TalonSRX(Consts.frontLeftTalon);
+		backLeft = new WPI_TalonSRX(Consts.backLeftTalon);
+		frontRight = new WPI_TalonSRX(Consts.frontRightTalon);
+		backRight = new WPI_TalonSRX(Consts.backRightTalon);
 		leftSpeedController = new SpeedControllerGroup (frontLeft, new SpeedController[] {backLeft});
 		rightSpeedController = new SpeedControllerGroup (frontRight, new SpeedController[] {backRight});
 		driveTrain = new DifferentialDrive(leftSpeedController, rightSpeedController);
