4e54b5fbd8bf72f1210f6ba79c7d0b18e6c1dbbf
deltaA380 <walsli@outlook.com>
2018-01-17 20:30:34 -0600

    added conts class

---
+++ src/org/usfirst/frc/team3630/robot/Contsts.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team3630.robot;
+
+public class Consts {
+	
+	// naming file convention can't reemeb
+	
+	// public static final int????
+	
+	// class secions  or organize by other catagories
+
+	
+	// DriveTrain 
+	
+	
+	
+	// Manipulators 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
