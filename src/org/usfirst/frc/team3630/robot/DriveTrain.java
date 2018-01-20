a99529e527a95edb0a2cc8f94d7b583ccc91103a
deltaA380 <walsli@outlook.com>
2018-01-19 21:22:02 -0600

    added pid write functionality
    added through class file like in wpilib pid controllor example. want to change. Need to add more comments

--- src/org/usfirst/frc/team3630/robot/DriveTrain.java
+++ src/org/usfirst/frc/team3630/robot/DriveTrain.java
@@ -2,20 +2,12 @@ package org.usfirst.frc.team3630.robot;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.wpilibj.PIDController;
-import edu.wpi.first.wpilibj.PIDSource;
-
-
-
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import com.kauailabs.navx.frc.AHRS;
-import edu.wpi.first.wpilibj.PIDController;
 
-public class DriveTrain  implements PIDOutput{
+public class DriveTrain  {
 	private XboxController _xBox;
 	AHRS ahrs;
 	
@@ -29,32 +21,35 @@ public class DriveTrain  implements PIDOutput{
     static final double kD = 0.00;
     static final double kF = 1;
     
-    static final double kToleranceDegrees = 2.0f;    
+    static final double kToleranceDegrees = .5f;    
     
     static final double kTargetAngleDegrees = 0f;
     
 	private  WPI_TalonSRX frontLeft, frontRight, backLeft, backRight;
 	private SpeedControllerGroup  leftSpeedController,rightSpeedController;
-	 PIDSource pidSource ;
+	// PIDSource pidSource ;
 	 DifferentialDrive driveTrain ;
+
 	public DriveTrain()  {
 		//calibrate navx !!!!!
 		 ahrs = new AHRS(SPI.Port.kMXP); 
-		 // pidSource = ahrs.setPIDSourceType(PIDSourceType.kDisplacement);
+
 		_xBox = new XboxController(Consts.xBoxComPort);
-		
+		// srx defin
 		frontLeft = new WPI_TalonSRX(Consts.frontLeftTalon);
 		backLeft = new WPI_TalonSRX(Consts.backLeftTalon);
 		frontRight = new WPI_TalonSRX(Consts.frontRightTalon);
 		backRight = new WPI_TalonSRX(Consts.backRightTalon);
-		
-		leftSpeedController = new SpeedControllerGroup (frontLeft, new SpeedController[] {backLeft});
+		//////////////////////////
+	
+	leftSpeedController = new SpeedControllerGroup (frontLeft, new SpeedController[] {backLeft});
 	rightSpeedController = new SpeedControllerGroup (frontRight, new SpeedController[] {backRight});
-
+////////////
 		driveTrain = new DifferentialDrive(leftSpeedController, rightSpeedController);
 		
-		   turnController = new PIDController(kP, kI, kD, kF, pidSource, this);
-	        turnController.setInputRange(-180.0f,  180.0f);
+		   turnController = new PIDController(kP, kI, kD, kF, ahrs, new MyPidOutput());
+	       
+		   turnController.setInputRange(-180.0f,  180.0f);
 	        turnController.setOutputRange(-1.0, 1.0);
 	        turnController.setAbsoluteTolerance(kToleranceDegrees);
 	        turnController.setContinuous(true);
@@ -62,16 +57,27 @@ public class DriveTrain  implements PIDOutput{
 
 	}
 	
+	
+	// init method for navx calibaration setting 
+	
+	
+	   /* This function is invoked periodically by the PID Controller, */
+    /* based upon navX MXP yaw angle input and PID Coefficients.    */
+    public void pidWrite(double output) {
+        rotateToAngleRate = output;
+    }
+
 	public void driveStraight() {
-		rotateToAngleRate= 0;
+
 		turnController.enable();
 		turnController.setSetpoint(kTargetAngleDegrees);
-
 		
 	}
 	
 	
 	
+	
+
 
 	public void driveTrainPeriodic() {
 		double speed = _xBox.getY(GenericHID.Hand.kLeft);
@@ -79,11 +85,17 @@ public class DriveTrain  implements PIDOutput{
 			driveTrain.arcadeDrive(speed, heading);
 			
 		}
+	
+
+	private class MyPidOutput implements PIDOutput {
+		@Override
+		public void pidWrite(double output) {
+			driveTrain.arcadeDrive(.5, output);
+		}
+
+	}
+}
+	
 		
 
-    /* This function is invoked periodically by the PID Controller, */
-    /* based upon navX MXP yaw angle input and PID Coefficients.    */
-    public void pidWrite(double output) {
-        rotateToAngleRate = output;
-    }
-}
+ 
