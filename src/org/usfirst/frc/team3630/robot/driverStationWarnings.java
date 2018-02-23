0d30059213cb43245c1772af7324d466beab449a
deltaA380 <walsli@outlook.com>
2018-02-23 17:05:58 -0600

    add comments. and made network tables periodic to be called irotovly in order to make sure network tables update

--- src/org/usfirst/frc/team3630/robot/driverStationWarnings.java
+++ src/org/usfirst/frc/team3630/robot/driverStationWarnings.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team3630.robot;
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.smartdashboard.*;
 import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.networktables.*;
 
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 
@@ -19,13 +20,26 @@ private double rollAngleDegree;
 static final double xPitchTollerance = 5.0;
 static final double yPitchTollerance = 5.0;
 PowerDistributionPanel panel;
-
+ NetworkTableEntry tipX, tipY;
 
 public driverStationWarnings (){
 	aHrs =  new AHRS(SPI.Port.kMXP);
 	panel = new PowerDistributionPanel(0);
 }
 
+/**
+ * network tables init needs to be called in robotINIT 
+ */
+public void networkTablesIinit() {
+	// creat intance of ds warnings and a table which gets put to the instance of network table 
+	NetworkTableInstance dsWarnings = NetworkTableInstance.getDefault();
+	NetworkTable myTable = dsWarnings.getTable("robotState");
+	tipX= myTable.getEntry("badPitch");
+	tipY= myTable.getEntry("badRoll");
+	
+}
+boolean badPitch= false;
+boolean badRoll= false;
 /**
  *  tip based on x axis rotaiton will print out warning to driver station
  */
@@ -37,8 +51,12 @@ public void tipOverX(){
 		System.out.print("WARNING ROBOTS DONT TIP ");
 		System.out.print(pitchAngleDegree);
 		System.out.print('\n');
+		badPitch= true ;
+		System.out.print(badPitch);
 		
 	}
+	netWorkPeriodic() ;
+	
 }
 
 /**
@@ -61,10 +79,25 @@ public void tipOverY(){
 		System.out.print("WARNING ROBOTS DONT TIP  ");
 		System.out.print(pitchAngleDegree);
 		System.out.print('\n');
+		badRoll= true;
+		System.out.print(badRoll);
 		
 		
 	}
-	
-	
+	// wouldn't we want to put this method here so we update netowrk tables as soon as it is called
+	netWorkPeriodic() ;
+
 }
+
+/**
+ * periodic which sets booleans based on which method is called or not
+ */
+public void netWorkPeriodic() {
+	
+	tipX.setBoolean(badPitch);
+	tipY.setBoolean(badRoll);
+	
+
+}
+
 }
