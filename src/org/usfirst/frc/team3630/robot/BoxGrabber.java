103256f18db4c5fcb18044eed454a21d39c2ff80
New Dell Computer <breckstampede@gmail.com>
2018-02-24 11:52:01 -0600

    troubleshoot changes

--- src/org/usfirst/frc/team3630/robot/BoxGrabber.java
+++ src/org/usfirst/frc/team3630/robot/BoxGrabber.java
@@ -7,339 +7,314 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.DigitalInput;
+
 public class BoxGrabber {
-	Timer manipTime;
-	
+
 	// enum difftent state= state (F, forward) (R, piston reverse)
-	//  lift 
+	// lift
 	public enum State {
-		SLIDEFORWARD, 
-		CLAMPOPEN, 
-		KICKFORWARD, 
-		LIFTUP, 
-		LIFTDOWN, 
-		CLAMPCLOSE,
-		SLIDEBACK, 
-		KICKRETRACT,
-		STOP,
-		INTAKE, 
-		DROPBOX,
-		SWITCHAUTOMATED,
-		SCALEAUTOMATED,
-		LIFTDOWNAUTOMATED,
-		LIFTUPAUTOMATED
-		
-		  
+		SLIDEFORWARD, CLAMPOPEN, KICKFORWARD, LIFTUP, LIFTDOWN, CLAMPCLOSE, SLIDEBACK, KICKRETRACT, STOP, INTAKE, DROPBOX, SWITCHAUTOMATED, SCALEAUTOMATED, LIFTDOWNAUTOMATED, LIFTUPAUTOMATED
+
 	}
+
 	private XboxController _xBox;
-//	private TalonSRX leftIntake, rightIntake;
+	// private TalonSRX leftIntake, rightIntake;
 	// name double solonoid
-	DoubleSolenoid slide,clamp,kick, lift;
+	DoubleSolenoid slide, clamp, kick, lift;
 	Compressor mainC;
 	// debug analog input
-	Boolean  liftUpEngaged, slideUpEngaged, slideOutEngaged, kickForwardEngaged, testOn, clampEnaged, kickReverseEngaged, liftDown, clampReverse;
+	Boolean liftUpEngaged, slideUpEngaged, slideOutEngaged, kickForwardEngaged, testOn, clampEnaged, kickReverseEngaged,
+			liftDown, clampReverse;
 	AnalogInput pressureLevel;
 	DigitalInput slideReverecheck, armsDownCheck;
-	
-public BoxGrabber(){
-	
-	// peramtors for double soelnoid pcm, in chanel, out chanel
-	// for detils on solondid asighning see output sheet i posted on slack 
-	manipTime= new Timer ();
-	slideReverecheck = new DigitalInput(1);
-	 armsDownCheck= new DigitalInput(2);
-	slide = new DoubleSolenoid(1,2,3);
-	kick	= new DoubleSolenoid(0,0, 1);
-	clamp = new DoubleSolenoid(0,2,3);
-	lift= new DoubleSolenoid(1,0, 1);
-	mainC= new Compressor(0);
-	pressureLevel= new AnalogInput(0);
-	_xBox = new XboxController(Consts.xBoxComPort);
-//	leftIntake = new TalonSRX(7);
-//
-//	rightIntake = new TalonSRX(8);
-//	leftIntake.setInverted(true);
-//	rightIntake.setInverted(true);
-}
 
-// goal to test automated buttons to see as reibale and then test replace buton function going to list them out tonight
-//plan intake togle 
-// box out button
-// switch buton lift up with tip back 
-// scale buton
-// general lift up button with ti back
-// clamp on box button
-public State xBox () {
-	// need to confirm buttons//  acyivates state for switch if button press is true 
-	if (_xBox.getXButton()== true ) {
-		return State.SCALEAUTOMATED;
+	public BoxGrabber() {
+
+		// peramtors for double soelnoid pcm, in chanel, out chanel
+		// for detils on solondid asighning see output sheet i posted on slack
+
+		// slideReverecheck = new DigitalInput(3);
+		// armsDownCheck= new DigitalInput(2);
+		slide = new DoubleSolenoid(1, 2, 3);
+		kick = new DoubleSolenoid(0, 0, 1);
+		clamp = new DoubleSolenoid(0, 2, 3);
+		lift = new DoubleSolenoid(1, 0, 1);
+		mainC = new Compressor(0);
+		pressureLevel = new AnalogInput(0);
+		_xBox = new XboxController(Consts.xBoxComPort);
+		// leftIntake = new TalonSRX(7);
+		//
+		// rightIntake = new TalonSRX(8);
+		// leftIntake.setInverted(true);
+		// rightIntake.setInverted(true);
 	}
-	else if (_xBox.getAButton() == true) {
-		return State.LIFTDOWNAUTOMATED;
+
+	// goal to test automated buttons to see as reibale and then test replace buton
+	// function going to list them out tonight
+	// plan intake togle
+	// box out button
+	// switch buton lift up with tip back
+	// scale buton
+	// general lift up button with ti back
+	// clamp on box button
+	public State xBox() {
+		// need to confirm buttons// acyivates state for switch if button press is true
+		if (_xBox.getXButton() == true) {
+			return State.SCALEAUTOMATED;
+		} else if (_xBox.getAButton() == true) {
+			return State.LIFTDOWNAUTOMATED;
+		} else if (_xBox.getBButton() == true) {
+			return State.LIFTUPAUTOMATED;
+		}
+		// else if (_xBox.getBumper(GenericHID.Hand.kRight)== true ) {
+		// return State.KICKRETRACT;
+		// }
+
+		else if (_xBox.getStartButton() == true) {
+			return State.DROPBOX;
+		} else if (_xBox.getBackButton() == true) {
+			return State.CLAMPOPEN;
+		}
+
+		else if (_xBox.getBumper(GenericHID.Hand.kLeft) == true) {
+			return State.CLAMPCLOSE;
+		}
+
+		else {
+			return State.STOP;
+		}
 	}
-else if (_xBox.getBButton()== true ) {
-		return State.LIFTUPAUTOMATED
-				;
+
+	/*
+	 * public void intake() { rightIntake.configNeutralDeadband(.1, 10);
+	 * leftIntake.configNeutralDeadband(.1, 10);
+	 * leftIntake.set(com.ctre.phoenix.motorcontrol.ControlMode.Follower,
+	 * rightIntake.getDeviceID());
+	 * rightIntake.set(com.ctre.phoenix.motorcontrol.ControlMode.PercentOutput,
+	 * _xBox.getTriggerAxis(GenericHID.Hand.kLeft));
+	 * 
+	 * }
+	 */
+
+	// each method for has a forward and reverse
+	// sets a bollean to true in order to know it has ben activated
+
+	public void kickForward() {
+
+		kick.set(DoubleSolenoid.Value.kForward);
+		kickForwardEngaged = true;
+
 	}
-//else if (_xBox.getBumper(GenericHID.Hand.kRight)== true  ) {
-//		return State.KICKRETRACT;
-//	}
 
-	else if (_xBox.getStartButton()== true ) {
-		return State.DROPBOX;
+	public void kickReverse() {
+		kick.set(DoubleSolenoid.Value.kReverse);
+		kickReverseEngaged = true;
+
 	}
-	else if (_xBox.getBackButton()== true ) {
-		return State.CLAMPOPEN;
+
+	public void armsUp() {
+		lift.set(DoubleSolenoid.Value.kForward);
+		liftUpEngaged = true;
+
 	}
-	
-	else if (_xBox.getBumper(GenericHID.Hand.kLeft)== true ) {
-		return State.CLAMPCLOSE;
+
+	public void armsDown() {
+		lift.set(DoubleSolenoid.Value.kReverse);
+		liftDown = true;
+
 	}
-	
 
-	
-	else {
-		return State.STOP;
+	public void clampOpen() {
+		clamp.set(DoubleSolenoid.Value.kForward);
+		clampEnaged = true;
 	}
-}
 
-
-
-/*public void intake() {
-	rightIntake.configNeutralDeadband(.1, 10);
-	leftIntake.configNeutralDeadband(.1, 10);
-	leftIntake.set(com.ctre.phoenix.motorcontrol.ControlMode.Follower, rightIntake.getDeviceID());
-	rightIntake.set(com.ctre.phoenix.motorcontrol.ControlMode.PercentOutput, _xBox.getTriggerAxis(GenericHID.Hand.kLeft));
-
-}*/
-
-// each method for has a forward and reverse 
-// sets a bollean to true in order to know it has ben activated 
-
-public void kickForward(){
-
-	kick.set(DoubleSolenoid.Value.kForward);
-	kickForwardEngaged= true;
-	
-}
-public void kickReverse(){
-	kick.set(DoubleSolenoid.Value.kReverse);
-	kickReverseEngaged= true;
-	
-}
-public void armsUp(){
-	lift.set(DoubleSolenoid.Value.kForward);
-	liftUpEngaged= true ;
-	
-}
-
-
-public void armsDown(){
-	lift.set(DoubleSolenoid.Value.kReverse);
-	liftDown= true ;
-	
-	
-}
-public void clampOpen() {
-	clamp.set(DoubleSolenoid.Value.kForward);
-	clampEnaged= true;
-}
-public void clampClose()
- {
-	clamp.set(DoubleSolenoid.Value.kReverse);
-	clampReverse=true;
-}
-// stop method for saftey 
-public void stop() {
-
-	clamp.set(DoubleSolenoid.Value.kOff);
-	lift.set(DoubleSolenoid.Value.kOff);
-	kick.set(DoubleSolenoid.Value.kOff);
-	
-}
-
-public void slideForward() {
-	slideUpEngaged= true;
-	slide.set(DoubleSolenoid.Value.kForward);
-}
-public void slideReverse() {
-	slideOutEngaged= true;
-	slide.set(DoubleSolenoid.Value.kReverse);
-}
-
-// psi method for presure sensor need to calibrate normaliesd voltage during testing 
-public double   compresorPSI() {
-	double sensorV= pressureLevel.getVoltage();
-	double  psi = 250 * (sensorV/ 5) -25 ;
-	
-	return psi;
-	
-	
-	
-
-	
-	
-	
-}
-public void lowPSIWarning() {
-	if(compresorPSI() < 60.0) {
-		System.out.println("WARNING robots dont have low presure but yourse dose");
-		System.out.print('\n');
+	public void clampClose() {
+		clamp.set(DoubleSolenoid.Value.kReverse);
+		clampReverse = true;
 	}
-}
 
-/**
- * method for geteing a box. Add timer delay curently for saftey 
- */
-public void kickoutBox() {
-	clampOpen(); 
-	Timer.delay(Consts.timeDelay);
-	kickForward();
-	Timer.delay(Consts.timeDelay);
-	kickReverse();
-	Timer.delay(Consts.timeDelay);
-	clampClose();
-	
-}
+	// stop method for saftey
+	public void stop() {
 
-/**
- * scale Auto method for lift up and dump box 
- */
-public void scaleAuto(){
-	 competionLiftUpScale();
-	Timer.delay(4);
-	kickoutBox();
-	Timer.delay(Consts.timeDelay);
-	armsDown();
-	
-}
-/**
- * lift up for scale method to drop box  for scale will go to full hight 
- */
-public void competionLiftUpScale() {
-	if (!slideReverecheck.get() && armsDownCheck.get() ) {
-		slideReverse();
+		clamp.set(DoubleSolenoid.Value.kOff);
+		lift.set(DoubleSolenoid.Value.kOff);
+		kick.set(DoubleSolenoid.Value.kOff);
+
 	}
-	else {
-		armsUp();
-		
+
+	public void slideForward() {
+		slideUpEngaged = true;
+		slide.set(DoubleSolenoid.Value.kForward);
 	}
-	
 
-	
-}
+	public void slideReverse() {
+		slideOutEngaged = true;
+		slide.set(DoubleSolenoid.Value.kReverse);
+	}
 
-/**saftey method for lift down. ensure robot can't be in forward state when the arms go down
- * will eventualy become driver lift down button
- */
-public void  liftDownRobotCompetion() {
-	if (!armsDownCheck.get() ) {
+	// psi method for presure sensor need to calibrate normaliesd voltage during
+	// testing
+	public double compresorPSI() {
+		double sensorV = pressureLevel.getVoltage();
+		double psi = 250 * (sensorV / 5) - 25;
+
+		return psi;
+
+	}
+
+	public void lowPSIWarning() {
+		if (compresorPSI() < 60.0) {
+			System.out.println("WARNING robots dont have low presure but yourse dose");
+			System.out.print('\n');
+		}
+	}
+
+	/**
+	 * method for geteing a box. Add timer delay curently for saftey
+	 */
+	public void kickoutBox() {
+		clampOpen();
+		Timer.delay(Consts.timeDelay);
+		kickForward();
+		Timer.delay(Consts.timeDelay);
+		kickReverse();
+		Timer.delay(Consts.timeDelay);
+		clampClose();
+
+	}
+
+	/**
+	 * scale Auto method for lift up and dump box
+	 */
+	public void scaleAuto() {
+		competionLiftUpScale();
+		Timer.delay(4);
+		kickoutBox();
+		Timer.delay(Consts.timeDelay);
 		armsDown();
-	
-	}
-	else  {
-		slideForward();
+
 	}
 
-}
-public void  liftUPRobotCompetion() {
-	if (!slideReverecheck.get() ) {
-		slideReverse();
+	/**
+	 * lift up for scale method to drop box for scale will go to full hight
+	 */
+	public void competionLiftUpScale() {
+		if (!slideReverecheck.get() && armsDownCheck.get()) {
+			slideReverse();
+
+		} else {
+			armsUp();
+
+		}
+
 	}
 
-	else {
-		armsUp();
-	
+	/**
+	 * saftey method for lift down. ensure robot can't be in forward state when the
+	 * arms go down will eventualy become driver lift down button
+	 */
+	public void liftDownRobotCompetion() {
+		if (!armsDownCheck.get()) {
+			armsDown();
+
+		} else {
+			slideForward();
+		}
+
 	}
-	
 
-}
-public void boxIntakeClamp() {
-	
-	
-}
+	public void liftUPRobotCompetion() {
+		if (!slideReverecheck.get()) {
+			slideReverse();
+		}
 
-/**
- * swutch auto metod need to test to fully develop
- */
-public void switchAuto() {
-	
-}
+		else {
+			armsUp();
 
-// manip diognostics output to smart doashboard for each pnumatic subsystem 
-public void manipulatorDianostics() {
-	testOn= true;
-	compresorPSI();
-	lowPSIWarning();
-	SmartDashboard.putNumber("Compresor PSI ",compresorPSI());
-	// presure switch output 
-	SmartDashboard.putBoolean("testOn", testOn);
-	SmartDashboard.putBoolean("liftgoing up", liftUpEngaged);
-	SmartDashboard.putBoolean(" Liftdown", liftDown);
-	SmartDashboard.putBoolean("kick On", kickForwardEngaged);
-	SmartDashboard.putBoolean("kick reversw", kickReverseEngaged);
-	SmartDashboard.putBoolean("slideForwardEngaged", slideUpEngaged);
-	SmartDashboard.putBoolean("slide reverse Engaged",slideOutEngaged );
-	
-	SmartDashboard.putBoolean("clamp forward Engaged",clampEnaged );
-	SmartDashboard.putBoolean("clamp reverse Engaged",clampReverse );
-	
-	
-	
-	
-}
+		}
 
-public void boxGraberPeriodic() {
-	// for testing 
-	//mainC.stop();
-	 liftUpEngaged=false;
-	 slideUpEngaged=false;
-	 slideOutEngaged=false;
-	 kickForwardEngaged=false;
-	 clampEnaged=false;
-	 kickReverseEngaged=false;
-	 liftDown=false;
-	 clampReverse=false;
-	manipulatorDianostics() ;
-	//intake();
-	   switch (xBox()) {
-       case SCALEAUTOMATED:
-    	   competionLiftUpScale();
-	
-           break;
-               
-       case DROPBOX:
-    	  kickoutBox() ;
-           break;
-                    
-       case LIFTDOWNAUTOMATED:
-    	   liftDownRobotCompetion();
-           break;
-           
-       case LIFTUPAUTOMATED:
-    	   liftUPRobotCompetion() ;
-    	   break;
-	   
-    
-		   
-	   case CLAMPCLOSE:
-		   clampClose();
-		   break;
-	   
-	   case CLAMPOPEN:
-		   clampOpen();
-		   break;
-	  
-	 
-	                                       
-       default:
-    	   // default to stop for saftey reasons 
-    	   stop();
-          
-           break;
-   }
-	
-}
+	}
 
+	public void boxIntakeClamp() {
+
+	}
+
+	/**
+	 * swutch auto metod need to test to fully develop
+	 */
+	public void switchAuto() {
+
+	}
+
+	// manip diognostics output to smart doashboard for each pnumatic subsystem
+	public void manipulatorDianostics() {
+		testOn = true;
+		compresorPSI();
+		lowPSIWarning();
+		SmartDashboard.putBoolean("slide back ", slideReverecheck.get());
+		SmartDashboard.putBoolean("ARE ARMS DOWN", armsDownCheck.get());
+		SmartDashboard.putNumber("Compresor PSI ", compresorPSI());
+		// presure switch output
+		SmartDashboard.putBoolean("testOn", testOn);
+		SmartDashboard.putBoolean("liftgoing up", liftUpEngaged);
+		SmartDashboard.putBoolean(" Liftdown", liftDown);
+		SmartDashboard.putBoolean("kick On", kickForwardEngaged);
+		SmartDashboard.putBoolean("kick reversw", kickReverseEngaged);
+		SmartDashboard.putBoolean("slideForwardEngaged", slideUpEngaged);
+		SmartDashboard.putBoolean("slide reverse Engaged", slideOutEngaged);
+
+		SmartDashboard.putBoolean("clamp forward Engaged", clampEnaged);
+		SmartDashboard.putBoolean("clamp reverse Engaged", clampReverse);
+
+	}
+
+	public void boxGraberPeriodic() {
+		// for testing
+		// mainC.stop();
+		liftUpEngaged = false;
+		slideUpEngaged = false;
+		slideOutEngaged = false;
+		kickForwardEngaged = false;
+		clampEnaged = false;
+		kickReverseEngaged = false;
+		liftDown = false;
+		clampReverse = false;
+		manipulatorDianostics();
+		// intake();
+		switch (xBox()) {
+		case SCALEAUTOMATED:
+			competionLiftUpScale();
+
+			break;
+
+		case DROPBOX:
+			kickoutBox();
+			break;
+
+		case LIFTDOWNAUTOMATED:
+			liftDownRobotCompetion();
+			break;
+
+		case LIFTUPAUTOMATED:
+			liftUPRobotCompetion();
+			break;
+
+		case CLAMPCLOSE:
+			clampClose();
+			break;
+
+		case CLAMPOPEN:
+			clampOpen();
+			break;
+
+		default:
+			// default to stop for saftey reasons
+			stop();
+
+			break;
+		}
+
+	}
 
 }
